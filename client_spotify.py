6c81ca52445f00ced817f7df347456f0898cb12f
Kartik Sinha <67892980+KartikYZ@users.noreply.github.com>
2020-12-20 18:32:32 +0530

    Add files via upload
    Client for Spotify API

---
+++ client_spotify.py
@@ -0,0 +1,22 @@
+import requests
+
+# spotify developer account credentials ->  Google Docs
+
+CLIENT_ID = "bbd7a9b053844b45b1bbe801383e8b2b"
+CLIENT_SECRET = ""  # Google Docs 
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
