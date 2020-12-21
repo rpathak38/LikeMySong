40a36ddaa7b0def158b23b5abfa63d7b9026ceed
g-abhi <57333505+g-abhi@users.noreply.github.com>
2020-12-20 20:17:22 -0600

    .py files for client and user
    testing easier in spotify_client_tester.ipynb Jupyter Notebook which has client and user combined

---
+++ spotify_client_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#import statements
+from spotify_client import *
+
+#instantiate Spotify API Client Object (SAPICO)
+client_id = '' # '' = your client_id
+client_secret = '' # '' = your client_secret
+spotify = SpotifyAPI(client_id, client_secret)
+
+#run methods on the SAPICO object (spotify)
+
