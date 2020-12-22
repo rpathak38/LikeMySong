f757b9aa21d0e048162ffc89d39e8148d9fcfd20
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-12-21 18:33:53 -0800

    react app

--- songrecommender/src/App.js
+++ songrecommender/src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { withRouter } from 'react-router-dom';
+import Main from './components/Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = withRouter((Main));
 
 export default App;
